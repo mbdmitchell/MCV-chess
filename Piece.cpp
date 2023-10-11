5bd547dd00c0184aa25f13b5ce93ef04ead76723
mbdmitchell <mbdmitchell@outlook.com>
2023-10-12 00:21:43 +0100

    Create Piece classes

---
+++ Piece.cpp
@@ -0,0 +1,159 @@
+#include "Piece.h"
+
+/// PIECE
+
+Piece::Piece(Piece::Colour colour) : colour{colour} {}
+
+Piece::Colour Piece::getColour() const {return colour; }
+
+/// PAWN
+
+Pawn::Pawn(Piece::Colour colour) : Piece(colour) { }
+
+Pawn::operator char() const {
+    char sprite = 'p';
+    return ((getColour() == Piece::Colour::WHITE) ? toupper(sprite, std::locale()) : sprite);
+}
+
+bool Pawn::isValidMovePath(const Location<> &source,
+                           const Location<> &destination,
+                           const Location<> &enPassantTargetSquare,
+                           const bool isCapture) const
+{
+    const bool isMoveForward = (getColour() == Piece::Colour::WHITE);
+    const bool isMovingInRightDirection = (isMoveForward == (getColour() == Piece::Colour::WHITE));
+    if (source == destination || !isMovingInRightDirection) return false;
+
+    if (!(Location<>::isVertical(source, destination) || Location<>::isDiagonal(source, destination))){
+        return false;
+    }
+
+    auto& [row, col] = source;
+    const auto [deltaRow, deltaColumn] = Location<>::calculateRowColumnDifferences(source, destination);
+
+    if (Location<>::isVertical(source, destination)) {
+        if (isCapture) return false;
+
+        if (abs(deltaRow) == 1) {
+            return true;
+        }
+        else if (abs(deltaRow) == 2) {
+            return ((row == 1 && getColour() == Piece::Colour::WHITE) || (row == 6 && getColour() == Piece::Colour::BLACK));
+        }
+        else {
+            return false;
+        }
+    }
+
+    // Location::isDiagonal(source, destination)) == true
+    const bool isEnPassant = enPassantTargetSquare == Location(source.get<0>().value(), destination.get<1>().value()); // Location(source.getRow(), destination.getColumn()));
+    return isEnPassant || (isCapture && abs(deltaRow) == 1);
+}
+
+std::unique_ptr<Piece> Pawn::clone() const {
+    return std::make_unique<Pawn>(*this);
+}
+
+/// BISHOP
+
+Bishop::Bishop(Piece::Colour colour) : Piece(colour) { }
+
+Bishop::operator char() const {
+    char sprite = 'b';
+    return ((getColour() == Piece::Colour::WHITE) ? toupper(sprite, std::locale()) : sprite);
+}
+
+bool Bishop::isValidMovePath(const Location<> &source,
+                             const Location<> &destination,
+                             const Location<> &enPassantTargetSquare,
+                             bool isCapture) const {
+    return Location<>::isDiagonal(source, destination);
+}
+
+std::unique_ptr<Piece> Bishop::clone() const {
+    return std::make_unique<Bishop>(*this);
+}
+
+/// KNIGHT
+
+Knight::Knight(Piece::Colour colour) : Piece(colour) { }
+
+Knight::operator char() const {
+    char sprite = 'n';
+    return ((getColour() == Piece::Colour::WHITE) ? toupper(sprite, std::locale()) : sprite);
+}
+
+bool Knight::isValidMovePath(const Location<> &source,
+                             const Location<> &destination,
+                             const Location<> &enPassantTargetSquare,
+                             bool isCapture) const {
+    return Location<>::isKnightMove(source, destination);
+}
+
+std::unique_ptr<Piece> Knight::clone() const {
+    return std::make_unique<Knight>(*this);
+}
+
+Rook::Rook(Piece::Colour colour) : Piece(colour) { }
+
+Rook::operator char() const {
+    char sprite = 'r';
+    return ((getColour() == Piece::Colour::WHITE) ? toupper(sprite, std::locale()) : sprite);
+}
+
+bool Rook::isValidMovePath(const Location<> &source,
+                           const Location<> &destination,
+                           const Location<> &enPassantTargetSquare,
+                           bool isCapture) const {
+    return (Location<>::isHorizontal(source, destination)
+        || Location<>::isVertical(source, destination));
+}
+
+std::unique_ptr<Piece> Rook::clone() const {
+    return std::make_unique<Rook>(*this);
+}
+
+/// QUEEN
+
+Queen::Queen(Piece::Colour colour) : Piece(colour) { }
+
+Queen::operator char() const {
+    char sprite = 'q';
+    return ((getColour() == Piece::Colour::WHITE) ? toupper(sprite, std::locale()) : sprite);
+}
+
+bool Queen::isValidMovePath(const Location<> &source,
+                            const Location<> &destination,
+                            const Location<> &enPassantTargetSquare,
+                            bool isCapture) const {
+    return (Location<>::isVertical(source, destination)
+        ||  Location<>::isDiagonal(source, destination)
+        ||  Location<>::isHorizontal(source, destination)
+    );
+}
+
+std::unique_ptr<Piece> Queen::clone() const {
+    return std::make_unique<Queen>(*this);
+}
+
+/// KING
+
+King::King(Piece::Colour colour) : Piece(colour) { }
+
+King::operator char() const {
+    char sprite = 'k';
+    return ((getColour() == Piece::Colour::WHITE) ? toupper(sprite, std::locale()) : sprite);
+}
+
+bool King::isValidMovePath(const Location<> &source,
+                           const Location<> &destination,
+                           const Location<> &enPassantTargetSquare,
+                           bool isCapture) const {
+
+    const auto& [rowDiff, colDiff] = Location<>::calculateRowColumnDifferences(source, destination);
+    return std::max(abs(rowDiff), abs(colDiff)) == 1 || (rowDiff == 0 && abs(colDiff) == 2);
+}
+
+std::unique_ptr<Piece> King::clone() const {
+    return std::make_unique<King>(*this);
+}
---
+++ Piece.h
@@ -0,0 +1,139 @@
+#pragma once
+
+#include <stdexcept>
+#include <locale>
+#include "Location.h"
+
+// NB: Included derived Piece classes here to speed up build-time on the *incredibly* slow machine I'm currently using
+class Piece {
+    /// ENUMS / STRUCTS
+public:
+    enum class Colour {WHITE, BLACK};
+    /// DATA MEMBERS
+protected:
+    Colour colour;
+
+    /// CONSTRUCTORS / DESTRUCTORS
+    explicit Piece(Colour colour);
+    virtual ~Piece() = default;
+
+    /// GETTERS
+    [[nodiscard]] Colour getColour() const;
+
+    /// OPERATORS
+    [[nodiscard]] virtual explicit operator char() const = 0;
+
+    /// VALIDATION.
+    [[nodiscard]] virtual bool isValidMovePath(const Location<> &source,
+                                               const Location<> &destination,
+                                               const Location<> &enPassantTargetSquare,
+                                               bool isCapture) const = 0;
+
+    /// MISC.
+    [[nodiscard]] virtual std::unique_ptr<Piece> clone() const = 0;
+};
+
+class Pawn : public Piece {
+public:
+    /// CONSTRUCTOR
+    explicit Pawn(Colour colour);
+
+    /// OPERATORS
+    [[nodiscard]] explicit operator char() const override;
+    /// VALIDATION
+    [[nodiscard]] bool isValidMovePath(const Location<> &source,
+                                       const Location<> &destination,
+                                       const Location<> &enPassantTargetSquare,
+                                       bool isCapture) const override;
+
+    /// MISC.
+    [[nodiscard]] std::unique_ptr<Piece> clone() const override;
+};
+
+
+class Bishop : public Piece {
+public:
+    /// CONSTRUCTOR
+    explicit Bishop(Colour colour);
+
+    /// OPERATORS
+    [[nodiscard]] explicit operator char() const override;
+    /// VALIDATION
+    [[nodiscard]] bool isValidMovePath(const Location<> &source,
+                                       const Location<> &destination,
+                                       const Location<> &enPassantTargetSquare,
+                                       bool isCapture) const override;
+
+    /// MISC.
+    [[nodiscard]] std::unique_ptr<Piece> clone() const override;
+};
+
+
+class Knight : public Piece {
+public:
+    /// CONSTRUCTOR
+    explicit Knight(Colour colour);
+
+    /// OPERATORS
+    [[nodiscard]] explicit operator char() const override;
+    /// VALIDATION
+    [[nodiscard]] bool isValidMovePath(const Location<> &source,
+                                       const Location<> &destination,
+                                       const Location<> &enPassantTargetSquare,
+                                       bool isCapture) const override;
+
+    /// MISC.
+    [[nodiscard]] std::unique_ptr<Piece> clone() const override;
+};
+
+class Rook : public Piece {
+public:
+    /// CONSTRUCTOR
+    explicit Rook(Colour colour);
+
+    /// OPERATORS
+    [[nodiscard]] explicit operator char() const override;
+    /// VALIDATION
+    [[nodiscard]] bool isValidMovePath(const Location<> &source,
+                                       const Location<> &destination,
+                                       const Location<> &enPassantTargetSquare,
+                                       bool isCapture) const override;
+
+    /// MISC.
+    [[nodiscard]] std::unique_ptr<Piece> clone() const override;
+};
+
+
+class Queen : public Piece {
+public:
+    /// CONSTRUCTOR
+    explicit Queen(Colour colour);
+
+    /// OPERATORS
+    [[nodiscard]] explicit operator char() const override;
+    /// VALIDATION
+    [[nodiscard]] bool isValidMovePath(const Location<> &source,
+                                       const Location<> &destination,
+                                       const Location<> &enPassantTargetSquare,
+                                       bool isCapture) const override;
+
+    /// MISC.
+    [[nodiscard]] std::unique_ptr<Piece> clone() const override;
+};
+
+class King : public Piece {
+public:
+    /// CONSTRUCTOR
+    explicit King(Colour colour);
+
+    /// OPERATORS
+    [[nodiscard]] explicit operator char() const override;
+    /// VALIDATION
+    [[nodiscard]] bool isValidMovePath(const Location<> &source,
+                                       const Location<> &destination,
+                                       const Location<> &enPassantTargetSquare,
+                                       bool isCapture) const override;
+
+    /// MISC.
+    [[nodiscard]] std::unique_ptr<Piece> clone() const override;
+};
