74610fea4dc6eb5cdb098a3ab18248d94a3b32c9
mbdmitchell <mbdmitchell@outlook.com>
2023-10-14 21:35:20 +0100

    Add Game constructors

--- GameController.h
+++ GameController.h
@@ -13,6 +13,9 @@ class GameController {
 
 /// CONSTRUCTORS
 
+    GameController() = default;
+    GameController(const GameController& rhs)
+        : game{rhs.game}, gameView{std::make_unique<GameViewCLI>()} { } // Change if implementing other `GameView`s
 /// GETTERS
 
 /// OPERATORS
