1d4bbe8e23690809d37b1cfba9186635accd6376
mbdmitchell <mbdmitchell@outlook.com>
2023-10-19 16:24:29 +0100

    Add default Player operator= and operator==

--- Player.h
+++ Player.h
@@ -6,8 +6,10 @@ class Player {
     Piece::Colour colour;
 public:
     explicit Player(Piece::Colour colour);
-
     Player(const Player& other);
 
     [[nodiscard]] Piece::Colour getColour() const;
+
+    Player& operator=(const Player& rhs) = default;
+    bool operator==(const Player& rhs) const = default;
 };
