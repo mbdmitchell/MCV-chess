a1b2e6705d7b3ac8e79d018de29e093aad0e6277
mbdmitchell <mbdmitchell@outlook.com>
2023-10-19 16:29:46 +0100

    Add Example main()

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 
-#include "Location.h"
+#include "GameController.h"
+
+// TODO: Idea: instead of unique_ptr's, construct needed objects in main() and classes have non-owning const references
+
 
 int main() {
-    Location l;
-    std::cout << Location<>::isHorizontal(Location{4,3}, Location{3,5});
-    return 0;
+    GameController g{};
+    g.setup();
+    g.initGameLoop();
+    return EXIT_SUCCESS;
 }
