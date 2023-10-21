5fffaba5d00a936fb875e5d5a94367df1e549273
Max Mitchell <95064392+mbdmitchell@users.noreply.github.com>
2023-10-21 18:52:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 // TODO: Idea: instead of unique_ptr's, construct needed objects in main() and classes have non-owning const references
 int main() {
     GameController g{};
-    g.setupSimple();
+    g.setup();
     g.initGameLoop();
     return EXIT_SUCCESS;
 }
