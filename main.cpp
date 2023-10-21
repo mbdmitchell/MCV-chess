2ea65c1629bd93f132dfcf0dd8e86c4ea51eed1f
Max Mitchell <95064392+mbdmitchell@users.noreply.github.com>
2023-10-21 20:36:11 +0100

    Add TODO

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 // TODO: Idea: instead of unique_ptr's, construct needed objects in main() and classes have non-owning const references
 // TODO: `noexcept` all the things!
+// TODO: `final` things that should be `final`
 int main() {
     GameController g{};
     g.setup();
