fd576b5bcacfcba3b29599b92aa12203853a16d4
Max Mitchell <95064392+mbdmitchell@users.noreply.github.com>
2023-10-26 23:01:51 +0100

    Add random idea for Move class in comments

--- main.cpp
+++ main.cpp
@@ -8,6 +8,24 @@
 // TODO: "Prefer pass-by-value for parameters that a function inherently would copy, but only if the parameter is 
 // of a type that supports move semantics. Otherwise, use reference-to-const parameters."
 
+/* -----------------------------------------------------------------------------
+
+IDEA - probably won't implement but thought it was fun
+
+struct SimpleMove {Location source, Location destination};
+struct CaptureMove {Location source, Location destination, bool enPassant};
+struct CastleMove {bool isKingside};
+
+class Move {
+    std::variant<SimpleMove, CaptureMove, CastleMove> details;
+public:
+    bool isSimpleMove() const {return details.index == 0;}
+    bool isCapturingMove() const {return details.index == 1;}
+    bool isCastlingMove() const {return details.index == 2;}
+};
+
+----------------------------------------------------------------------------- */
+
 int main() {
     GameController g{};
     g.setup();
